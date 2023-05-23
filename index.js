59ebdb0f961cfeca12475034ff3b6bd735348835
ldrennan279 <leedrennan279@gmail.com>
2023-05-23 07:04:06 +0100

    4th commit
    Updated to include data from firebase

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: 'https://we-are-the-champions-fe824-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -15,36 +15,39 @@ const toInputEl = document.getElementById('to-input')
 const publishBtnEl = document.getElementById('publish-btn')
 const feedSection = document.getElementById('feed_section')
 
+onValue(endorsementListDB,  function(snapshot){
+    let endorsementArray = Object.values(snapshot.val())
+    endorsementArray.forEach((item)=>{
+          feedSection.innerHTML += `
+            <div class="endorsementBox">
+                <h3 class="endorsement-to">${item.personTo}</h3>
+                <p class="endorsement-text">${item.endorsement}</p>
+                <div class="likes-section">
+                    <h3 class="endorsement-from">${item.personFrom}</h3>
+                    <p class="likes">Likes 0</p>
+                </div>
+            </div>
+        
+        `
+    })
+    })
+
 publishBtnEl.addEventListener('click', ()=>{
     let endorsementText = mainInputEl.value
     let fromText = fromInputEl.value
     let toText = toInputEl.value
    
-    let endorsementObject = {
+    if(endorsementText && fromText && toText){
+       let endorsementObject = {
         personFrom: `${fromText}`,
         personTo: `${toText}`,
         endorsement: `${endorsementText}`
+        }
+        push(endorsementListDB, endorsementObject) 
     }
-    addEndorsementToFieldSection()
-    // push(endorsementListDB, endorsementObject)
-
+    
     clearInputFields()
 })
-
-// create function that pulls data from database and adds to the HTML feed section.
-function addEndorsementToFieldSection(){
-    feedSection.innerHTML += `
-        <div class="endorsementBox">
-            <h3 class="endorsement-to">To frank</h3>
-            <p class="endorsement-text">lee was here last night and had a really good time. he want to play the play station all night long!!!</p>
-            <div class="likes-section">
-                <h3 class="endorsement-from">From Andy</h3>
-                <p class="likes">Likes 0</p>
-            </div>
-        </div>
-    
-    `
-}
 
 function clearInputFields(){
     mainInputEl.value = ""
