08a972f1b87d07ab7a02d11a99c661018f491fc6
ldrennan279 <leedrennan279@gmail.com>
2023-05-28 06:30:34 +0100

    5th commit

--- index.js
+++ index.js
@@ -15,23 +15,6 @@ const toInputEl = document.getElementById('to-input')
 const publishBtnEl = document.getElementById('publish-btn')
 const feedSection = document.getElementById('feed_section')
 
-onValue(endorsementListDB,  function(snapshot){
-    let endorsementArray = Object.values(snapshot.val())
-    endorsementArray.forEach((item)=>{
-          feedSection.innerHTML += `
-            <div class="endorsementBox">
-                <h3 class="endorsement-to">${item.personTo}</h3>
-                <p class="endorsement-text">${item.endorsement}</p>
-                <div class="likes-section">
-                    <h3 class="endorsement-from">${item.personFrom}</h3>
-                    <p class="likes">Likes 0</p>
-                </div>
-            </div>
-        
-        `
-    })
-    })
-
 publishBtnEl.addEventListener('click', ()=>{
     let endorsementText = mainInputEl.value
     let fromText = fromInputEl.value
@@ -45,8 +28,36 @@ publishBtnEl.addEventListener('click', ()=>{
         }
         push(endorsementListDB, endorsementObject) 
     }
-    
     clearInputFields()
+})
+
+onValue(endorsementListDB,  function(snapshot){
+    let endorsementArray = Object.values(snapshot.val())
+    let endorsementArrayReverse = endorsementArray.reverse()
+    feedSection.innerHTML = ""
+    endorsementArrayReverse.forEach((item)=>{
+          feedSection.innerHTML += `
+            <div class="endorsementBox">
+                <h3 class="endorsement-to">${item.personTo}</h3>
+                <p class="endorsement-text">${item.endorsement}</p>
+                <div class="likes-section">
+                    <h3 class="endorsement-from">${item.personFrom}</h3>
+                    <p class="likes">
+                        <img src="images/heart.png" alt="Heart Logo" width="14px" id="heart">
+                        0
+                    </p>
+                </div>
+            </div>
+        
+        `
+    })
+    })
+
+document.addEventListener('click', (e)=>{
+    if(e.target.id === 'heart'){
+        let heartSeleceted = heart
+        heartSeleceted.stlye.color = 'red'
+    }
 })
 
 function clearInputFields(){
