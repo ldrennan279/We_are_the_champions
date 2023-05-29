a8a1fcfd8cb79ea3525299108eda7ec8679e85eb
ldrennan279 <leedrennan279@gmail.com>
2023-05-29 06:10:44 +0100

    6th Commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import { v4 as uuidv4 } from "uuid";
+
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
 import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 
 const appSettings = {
     databaseURL: 'https://we-are-the-champions-fe824-default-rtdb.europe-west1.firebasedatabase.app/'
@@ -15,13 +18,17 @@ const toInputEl = document.getElementById('to-input')
 const publishBtnEl = document.getElementById('publish-btn')
 const feedSection = document.getElementById('feed_section')
 
+
 publishBtnEl.addEventListener('click', ()=>{
     let endorsementText = mainInputEl.value
     let fromText = fromInputEl.value
     let toText = toInputEl.value
+    let feedMessageId = uuidv4()
+    console.log(feedMessageId)
    
     if(endorsementText && fromText && toText){
        let endorsementObject = {
+        uuid: `${uuidv4()}`,
         personFrom: `${fromText}`,
         personTo: `${toText}`,
         endorsement: `${endorsementText}`
@@ -43,8 +50,8 @@ onValue(endorsementListDB,  function(snapshot){
                 <div class="likes-section">
                     <h3 class="endorsement-from">${item.personFrom}</h3>
                     <p class="likes">
-                        <img src="images/heart.png" alt="Heart Logo" width="14px" id="heart">
-                        0
+                        <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11" viewBox="0 0 24 24"><path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z" id="${item}" /></svg>
+                        <span>0</span>
                     </p>
                 </div>
             </div>
@@ -52,12 +59,10 @@ onValue(endorsementListDB,  function(snapshot){
         `
     })
     })
+    
 
 document.addEventListener('click', (e)=>{
-    if(e.target.id === 'heart'){
-        let heartSeleceted = heart
-        heartSeleceted.stlye.color = 'red'
-    }
+   
 })
 
 function clearInputFields(){
